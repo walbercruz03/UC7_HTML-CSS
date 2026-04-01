31328cfa785c5f9218cddec2fa71ad573367e848
WALBER VICTOR CRUZ DE LIMA <a95827678@rn.senac.local>
2026-03-31 21:18:13 -0300

    novo_js

---
+++ projeto/html/test.py
@@ -0,0 +1,13 @@
+from datetime import timedelta
+from odoo import models, fields, api
+from oddo.exceptions import ValidationError
+
+
+from oddo import models, fields, api
+
+from oddo import models, fields, api
+
+from odoo import mpdels, fields, api
+
+from oddo import models, fields, api
+from datetime import timedelta
